ddefe92638e3bc0fdfeb0e86e8bfcb9bf13dad93
SunChero <sunshero@msn.com>
2019-09-07 23:09:48 -0400

    fix env vars  and added gitignore

--- boot.go
+++ boot.go
@@ -21,8 +21,8 @@ func boot() {
 	//init goth authentication providers
 	//gothic.Store = sessions.NewCookieStore([]byte("secret"))
 	goth.UseProviders(
-		github.New("768a17f699742fe6d678", "013755247a126c8ae6d950622969965cee6daac1", "http://localhost:3000/auth/github/callback"),
-		twitter.New("amVZhycw7xFLQXhc4UrpvOwvr", "5viWz2940venjYzGmfKucYfQh6FVOPxDCKgU3b2hUHXVEML1gg", "http://localhost:3000/auth/twitter/callback"),
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+		twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://localhost:3000/auth/twitter/callback"),
 	)
 
 	//init db
